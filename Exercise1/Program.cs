356030523cfae5f8a09396ea5451d0d4ad30ab4b
USER <USER@DESKTOP-ORGL5LP>
2016-12-07 01:36:00 +0400

    Добавьте файлы проекта.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] iARRAY = new int[10, 10];
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    iARRAY[i, j] = random.Next(100);
+                    Console.Write("{0,9}", iARRAY[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("max {0}", iARRAY.Max());
+
+        }
+    }
+}
---
+++ Exersice2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersice2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] iARRAY = new int[10, 10];
+            int imax, jmax, imin, jmin;
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    iARRAY[i, j] = random.Next(1000);
+                }
+            }
+
+            double max = iARRAY[0, 0];
+            for (int i = 0; i < iARRAY.GetLength(0); i++)
+            {
+                for (int j = 0; j < iARRAY.GetLength(1); j++)
+                {
+                    if (max < iARRAY[i, j])
+                    {
+                        max = iARRAY[i, j];
+                        imax = i, jmax = j;
+                    }
+                }
+            }
+
+            double min = iARRAY[0, 0];
+            for (int i = 0; i < iARRAY.GetLength(0); i++)
+            {
+                for (int j = 0; j < iARRAY.GetLength(1); j++)
+                {
+                    if (min > iARRAY[i, j])
+                    {
+                        min = iARRAY[i, j];
+                        imin = i, imin = j;
+                    }
+                }
+            }
+            Console.WriteLine("Минимальное значение {0}, Индекс: {1}{2}. Максимальное значение {3}, Индекс: {4}{5}", min, imax, jmax, max);
+            Console.ReadLine();
+        }
+    }
+}
